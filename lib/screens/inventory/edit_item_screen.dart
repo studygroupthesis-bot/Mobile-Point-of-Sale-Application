1c8b565e19920824eaccdb4d409b65792e2120f9
Thesis <studygroup.thesis@gmail.com>
2026-03-15 15:52:11 +0800

    Barcode create and edit save data, while transaction camera is on dashboard

--- lib/screens/inventory/edit_item_screen.dart
+++ lib/screens/inventory/edit_item_screen.dart
@@ -8,7 +8,7 @@ import 'package:image_picker/image_picker.dart';
 
 import '../../services/cloudinary_service.dart';
 import 'add_stock_screen.dart';
-import '../../screens/transaction/barcode_scanner_screen.dart';
+import 'barcode_scanner_screen.dart';
 
 enum SoldBy { each, weight }
 
